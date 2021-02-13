5a43a663f168cfef577476ad0be05aac6c8070c4
6z3 <69752110+6z3@users.noreply.github.com>
2021-02-13 20:44:46 +0400

    Update Checker Tellonym.py

--- Checker Tellonym/Checker Tellonym.py
+++ Checker Tellonym/Checker Tellonym.py
@@ -1,53 +1,59 @@
+#by king berlin | @680068
 import requests
 import os
-#680068
+
+count = 0
+
 banner = ('''
-  _______ ______ _      _      ____  _   ___     ____  __
- |__   __|  ____| |    | |    / __ \| \ | \ \   / /  \/  |
-    | |  | |__  | |    | |   | |  | |  \| |\ \_/ /| \  / |
-    | |  |  __| | |    | |   | |  | | . ` | \   / | |\/| |
-    | |  | |____| |____| |____ |__| | |\  |  | |  | |  | |
-    |_|  |______|______|______\____/|_| \_|  |_|  |_|  |_|
-
-                 Coded by | @680068
-
-
+[!] Follow Me In Instagram : @680068
+  _______ ______ _      _         _____ _    _ ______ _____ _  ________ _____  
+ |__   __|  ____| |    | |       / ____| |  | |  ____/ ____| |/ /  ____|  __ \ 
+    | |  | |__  | |    | |      | |    | |__| | |__ | |    | ' /| |__  | |__) |
+    | |  |  __| | |    | |      | |    |  __  |  __|| |    |  < |  __| |  _  / 
+    | |  | |____| |____| |____  | |____| |  | | |____ |____| . \| |____| | \ \ 
+    |_|  |______|______|______|  \_____|_|  |_|______\_____|_|\_\______|_|  \_\
+                                                                               
+                          Coded by | @680068        
 ''')
 print(banner)
 
-print(" ")
 
-input("Press, Enter To Start :")
-username = 'user.txt'
-use = username
+
+file0 = input("[~] Enter File Name :")
+
+input("\n[~] Press, Enter To Start")
+
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'cookie': '__cfduid=d0ffc45fa1efa9e0d736b1c14bb6c594a1605979176; _ga=GA1.2.821290333.1605979177; G_ENABLED_IDPS=google; __gads=ID=c7705da3c1f492a3:T=1606880061:S=ALNI_MZomN5KxOkG_i59jEZaJOEsiBQi6g; _gid=GA1.2.725225671.1606979880; cf_clearance=dcd41d1895597ca22ffa90136382f60ddea9e6e2-1606979881-0-150; __rtgt_sid=ki8ibzq4bp1k2d; _gat=1',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'cookie': '__cfduid=d9a45d5a0d1610e2a1abf30dac99aa7681613198934; _ga=GA1.2.1551905763.1613198948; _gid=GA1.2.767379040.1613198948; __cf_bm=ee7ac75044152eb1d2ad2f130402990e23eb0cda-1613232593-1800-AQ08xpweTVOHARq3LOG08YITD6T71cgZNqq41jhl8gtsnAg3eEVY5jwwMSOZloDO+EAv3V2SX2xaOSJKBnM5cBEPYNza54sWGauQeMsRKPnrnKMf2jKcta+uoo5ZWQV9ag==; _gat=1; __rtgt_sid=kl3x176qgjlsv2',
+    'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
+    'sec-fetch-site': 'none',#berlin
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
-}
-file = open(username, "r")
-while True:
-  Check = file.readline().split('\n')[0]
-  tiillog = f'https://tellonym.me/{Check}'
-  rq = requests.get(tiillog, headers=headers)
-  if rq.status_code == 404:
-      count += 1
-      print("{}: {}".format(count, Check.strip()) + " | Available")
-      with open('usersfound.txt', 'a') as x:
-             x.write(Check + '\n')
-      
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+    }
 
-  elif rq.status_code == 200:
-      
+file = open(file0, "r")
+
+while True:
+  che = file.readline().split('\n')[0]
+  url = f'https://tellonym.me/{che}'
+  req = requests.get(url, headers=headers)
+  if (che == ""):
+      print(" ")
+      input("Press, Enter To Close Program .. ")
+      exit(0)
+  if req.status_code == 404:
       count += 1
-      
-      print("{}: {}".format(count, Check.strip()) + " | NoT Available")
-      if (Check == ""):
-          break
+      print("{}: {}".format(count, che.strip()) + " | Available")
+      with open('Available @680068.txt', 'a') as x:
+             x.write(che + '\n')
+  else:
+      count +=1
+      print("{}: {}".format(count, che.strip()) + " | Not Available")
